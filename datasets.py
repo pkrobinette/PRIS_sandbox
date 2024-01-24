0030a611f30671a2bd2d0d69278784969bea5b0a
Preston Robinette <preston.k.robinette@vanderbilt.edu>
2024-01-24 01:27:26 +0000

    add imagenet dataset for 64x64

--- datasets.py
+++ datasets.py
@@ -4,12 +4,83 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as T
 import config as c
 from natsort import natsorted
+import random
+
+random.seed(10)
+
+TRANSFORMS = {
+    "imagenet": {
+        "transform": T.Compose([
+            T.RandomHorizontalFlip(),
+            T.RandomVerticalFlip(),
+            T.Resize((c.cropsize, c.cropsize), antialias=None),
+            T.ToTensor()]),
+        "tranform_val": T.Compose([
+            T.Resize((c.cropsize, c.cropsize), antialias=None),
+            T.ToTensor()])
+    },
+    "div2k" : {
+        "transform_imagenet": T.Compose([
+            T.RandomHorizontalFlip(),
+            T.RandomVerticalFlip(),
+            T.RandomCrop(c.cropsize),
+            T.ToTensor()]),
+        "transform_val": T.Compose([
+            T.CenterCrop(c.cropsize_val),
+            T.ToTensor()]),
+    }
+}
 
 
 def to_rgb(image):
     rgb_image = Image.new("RGB", image.size)
     rgb_image.paste(image)
     return rgb_image
+
+class ImageNetDataset(Dataset):
+    def __init__(self, 
+                transforms_=None, 
+                mode = "train"
+            ):
+        self.transforms = transforms_
+        #
+        # Init dataset, triggers, and responses
+        #
+        print(c.TRAIN_PATH)
+        if mode == 'train':
+            # train
+            self.dataset = natsorted(sorted(glob.glob(c.TRAIN_PATH + "/*." + c.format_train)))
+            if c.num_train is not None:
+                num_images = min(c.num_train, len(self.dataset))
+                random.shuffle(self.dataset)
+                self.dataset = self.dataset[:num_images]
+
+        else:
+            # test
+            self.dataset = sorted(glob.glob(c.VAL_PATH + "/*." + c.format_val))
+            if c.num_val is not None:
+                num_images = min(c.num_val, len(self.dataset))
+                random.shuffle(self.dataset)
+                self.dataset = self.dataset[:num_images]
+
+    def __len__(self):
+        """
+        Get length of dataset
+        """
+        return len(self.dataset)
+
+
+    def __getitem__(self, idx):
+        """
+        Get image, trigger, and trigger response
+        """
+        image_name = self.dataset[idx]
+        image = Image.open(image_name)
+
+        if self.transforms:
+            image = self.transforms(image)
+        
+        return image
 
 
 
@@ -43,42 +114,43 @@ class Hinet_Dataset(Dataset):
 
         else:
             return len(self.files)
-transform = T.Compose([
-    T.RandomHorizontalFlip(),
-    T.RandomVerticalFlip(),
-    T.RandomCrop(c.cropsize),
-    T.ToTensor()
-])
-
-transform_val = T.Compose([
-    T.CenterCrop(c.cropsize_val),
-    T.ToTensor(),
-])
 
 
-# Training data loader
-# trainloader = DataLoader(
-#     Hinet_Dataset(transforms_=transform, mode="train"),
-#     batch_size=c.batch_size,
-#     shuffle=True,
-#     pin_memory=True,
-# #    num_workers=8,
-#     drop_last=True
-# )
-trainloader = DataLoader(
-    Hinet_Dataset(transforms_=transform, mode="train"),
-    batch_size=c.batch_size,
-    shuffle=True,
-    pin_memory=True,
-    num_workers=8,
-    drop_last=True
-)
-# Test data loader
-testloader = DataLoader(
-    Hinet_Dataset(transforms_=transform_val, mode="val"),
-    batch_size=c.batchsize_val,
-    shuffle=False,
-    pin_memory=True,
-    num_workers=1,
-    drop_last=True
-)
+if c.dataset == "imagenet":
+    trainloader = DataLoader(
+        ImageNetDataset(transforms_=TRANSFORMS["imagenet"]["transform"], mode="train"),
+        batch_size=c.batch_size,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=8,
+        drop_last=True
+    )
+
+    testloader = DataLoader(
+        Hinet_Dataset(transforms_=TRANSFORMS["imagenet"]["transform_val"], mode="val"),
+        batch_size=c.batchsize_val,
+        shuffle=False,
+        pin_memory=True,
+        num_workers=1,
+        drop_last=True
+    )
+
+elif c.dataset == "div2k":
+    trainloader = DataLoader(
+        Hinet_Dataset(transforms_=TRANSFORMS["div2k"]["transform_val"], mode="train"),
+        batch_size=c.batch_size,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=8,
+        drop_last=True
+    )
+    # Test data loader
+    testloader = DataLoader(
+        Hinet_Dataset(transforms_=TRANSFORMS["div2k"]["transform_val"], mode="val"),
+        batch_size=c.batchsize_val,
+        shuffle=False,
+        pin_memory=True,
+        num_workers=1,
+        drop_last=True
+
+    )
